39f7493885c49e6290eda749a9cd0fe778e05d0f
Alexander Arenas <arenasocampoalexander@gmail.com>
2022-07-21 23:13:40 -0500

    agregue clase persona

--- RetoEleccionesGp08/src/Clases/ClsVotante.java
+++ RetoEleccionesGp08/src/Clases/ClsVotante.java
@@ -11,7 +11,7 @@ package Clases;
 public class ClsVotante extends ClsPersona{
     private String correoElectronico;
 
-    public ClsVotante(String correoElectronico, String tipoDocumento, int numeroDocumento, String nombre, String telefono) {
+    public ClsVotante(String correoElectronico, String tipoDocumento, String numeroDocumento, String nombre, String telefono) {
         super(tipoDocumento, numeroDocumento, nombre, telefono);
         this.correoElectronico = correoElectronico;
     }
