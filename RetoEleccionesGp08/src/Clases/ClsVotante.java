ddc44552fb269e3697e1129080937aaaf5ae39fa
Alexander Arenas <arenasocampoalexander@gmail.com>
2022-07-21 23:09:08 -0500

    vista basica

---
+++ RetoEleccionesGp08/src/Clases/ClsVotante.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author arena
+ */
+public class ClsVotante extends ClsPersona{
+    private String correoElectronico;
+
+    public ClsVotante(String correoElectronico, String tipoDocumento, int numeroDocumento, String nombre, String telefono) {
+        super(tipoDocumento, numeroDocumento, nombre, telefono);
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
