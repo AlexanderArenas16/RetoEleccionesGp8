189ce28dfa63ab655dc5976e72f9594356868342
Oacegal79 <106786790+Oacegal79@users.noreply.github.com>
2022-07-25 21:18:12 -0500

    Paquete candidato
    Se crean clases, controladores, vistas, modelo, y jdbc para candidato

---
+++ RetoEleccionesGp08/src/Clases/ClsJdbc.java
@@ -0,0 +1,36 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author orlandoacevedo
+ */
+public class ClsJdbc {
+
+    String driver = "com.mysql.cj.jdbc.Driver";
+    String usuario = "root";
+    String contrasenia = "";
+    String bd = "bd_elecciones";
+    String url = "jdbc:mysql://localhost:3306/" + this.bd;  
+   
+    public Connection conexion;
+    
+    
+    public void CrearConexion(){
+    
+        try {
+            Class.forName(driver);
+            this.conexion = DriverManager.getConnection(url, usuario, contrasenia);
+            
+            if(this.conexion != null) {
+                  System.out.println("Conexión exitosa");
+            } 
+                    
+        } catch(Exception error){
+            System.out.println("Ocurrió un error al conectar: " + error.getMessage());
+        }
+    }
+}
